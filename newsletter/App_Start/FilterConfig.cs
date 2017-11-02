7228ac1a82e4f4b2414897173beb0a3558fc51c0
annie <anniie.mukherji@gmail.com>
2017-11-02 15:02:14 +0530

    Initial Commit With All New Files and Folders

---
+++ newsletter/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace newsletter
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
