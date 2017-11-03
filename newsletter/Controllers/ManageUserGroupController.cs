dce8c8486fbff271bfc191a5268b07dd14c1b1a8
annie <anniie.mukherji@gmail.com>
2017-11-03 14:04:19 +0530

    Some more UI changes.

---
+++ newsletter/Controllers/ManageUserGroupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace newsletter.Controllers
+{
+    public class ManageUserGroupController : Controller
+    {
+        // GET: ManageUserGroup
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ newsletter/Controllers/SendNewsletterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace newsletter.Controllers
+{
+    public class SendNewsletterController : Controller
+    {
+        // GET: SendNewsletter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
